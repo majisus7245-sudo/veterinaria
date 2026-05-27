9dc8d57d7db2da1e15dd18598d27966c4ed0ed4f
Manuel de Jesus Inzunza Lara <majisus7245@gmail.com>
2026-05-26 19:45:19 -0700

    comit de prueba

--- src/models/Usuario.java
+++ src/models/Usuario.java
@@ -5,10 +5,7 @@ public class Usuario {
 	private int id;
 	private String nombre, password, rol, estado;
 	
-	public Usuario(String nombre, String password) {
-		this.nombre = nombre;
-		this.password = password;
-	}
+	public Usuario() {}
 	
 	public Usuario(int id, String nombre, String password, String rol, String estado) {
 		this.id = id;
@@ -17,5 +14,47 @@ public class Usuario {
 		this.rol = rol;
 		this.estado = estado;
 	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
 
 }
