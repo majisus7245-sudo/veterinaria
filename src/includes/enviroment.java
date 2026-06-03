25ea3fa84c5263fe838ce699805cb8c1ea690594
Manuel de Jesus Inzunza Lara <majisus7245@gmail.com>
2026-06-02 17:24:24 -0700

    ...

--- src/includes/enviroment.java
+++ src/includes/enviroment.java
@@ -1,7 +1,7 @@
 package includes;
 
 public class enviroment {
-	public static final String DB_NAME = "Veterinariaaaa";
+	public static final String DB_NAME = "Veterinaria";
 	public static final String DB_SERVER = "TP-MANUEL\\PC_2";
 	public static final String DB_USER = "tec";
 	public static final String DB_PASSWORD = "123";
